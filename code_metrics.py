b600c85942b0fe4e644299888fa6dc3abfd4d4f5
Shigeaki Matsumura <matsu911@gmail.com>
2012-01-22 17:47:41 +0900

    added class Processor and Java classes and some keywords

--- code_metrics.py
+++ code_metrics.py
@@ -2,31 +2,37 @@ import os
 import re
 from optparse import OptionParser
 
-parser = OptionParser("usage: %prog [options] dir")
-(options, args) = parser.parse_args()
-
-dir = args[0]
-ext = ".java"
 ret_code = '\n'
-keywords = ['if', 'else']
 
-def remove_comment(code):
-    s = re.sub(r'/\*(?:.|%s)*?\*/' % ret_code, r'', code, flags=re.M)
-    s = re.sub(r'^\s*//.*%s' % ret_code, r'', s, flags=re.M)
-    return re.sub(r'//.*', r'', s)
+class Processor(object):
+    def metric(self, f):
+        code = open(f).read()
+        code_no_comment = self.remove_comment(code)
+        # print code_no_comment
+        return dict([('loc', len(code.split(ret_code))),
+                     ('loc_no_comment', len(code_no_comment.split(ret_code)))] + 
+                    [(k, len(re.findall(r'\b%s\b' % k, code_no_comment))) for k in self.keywords])
 
-def metric(f):
-    code = open(f).read()
-    code_no_comment = remove_comment(code)
-    # print code_no_comment
-    return dict([('loc', len(code.split(ret_code))),
-                 ('loc_no_comment', len(code_no_comment.split(ret_code)))] + 
-                [(k, len(re.findall(r'\b%s\b' % k, code_no_comment))) for k in keywords])
+class Java(Processor):
+    keywords = ['if', 'else', 'try', 'catch']
+    def remove_comment(self, code):
+        s = re.sub(r'/\*(?:.|%s)*?\*/' % ret_code, r'', code, flags=re.M)
+        s = re.sub(r'^\s*//.*%s' % ret_code, r'', s, flags=re.M)
+        return re.sub(r'//.*', r'', s)
 
-colnames = ['loc', 'loc_no_comment'] + keywords
-print ",".join(['path'] + colnames)
-for root, dirs, files in os.walk(dir, topdown=False):
-    for path in [os.path.join(root, f) for f in files if os.path.splitext(f)[1] == ext]:
-        data = metric(path)
-        print ",".join([path] + [str(data[k]) for k in colnames])
+
+processors = { ".java" : Java() }
+
+if __name__ == '__main__':
+    parser = OptionParser("usage: %prog [options] dir")
+    (options, args) = parser.parse_args()
+    dir = args[0]
+    ext = '.java'               # only java supported for now
+    processor = processors[ext]
+    colnames = ['loc', 'loc_no_comment'] + processor.keywords
+    print ",".join(['path'] + colnames)
+    for root, dirs, files in os.walk(dir, topdown=False):
+        for path in [os.path.join(root, f) for f in files if os.path.splitext(f)[1] == ext]:
+            data = processor.metric(path)
+            print ",".join([path] + [str(data[k]) for k in colnames])
 
